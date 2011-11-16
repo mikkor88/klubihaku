615ac7c5c14095f3abd4566e56be79c2f48ecfaa
Mikko Ruuskanen <mikko.ruuskanen@mbnet.fi>
2011-11-17 00:16:04 +0200

    compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Klubihaku::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+	config.assets.precompile += %w( *.css *.js )
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
