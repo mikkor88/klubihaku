fe3d483be26f1abc4fb41916c90336a969b883d5
Mikko Ruuskanen <mikko.ruuskanen@mbnet.fi>
2011-11-21 23:46:36 +0200

    2nd try

---
+++ vendor/assets/javascript/external.js
@@ -0,0 +1,5 @@
+//= require gmaps4rails/bing.js
+//= require gmaps4rails/googlemaps.js
+//= require gmaps4rails/mapquest.js
+//= require gmaps4rails/openlayers.js
+//= require gmaps4rails/all_apis.js
